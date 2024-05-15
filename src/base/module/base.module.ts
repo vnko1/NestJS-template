ee68905d7aca7d8d402ce855a4f4cdec444e2da4
Andrii Valenko <andreyvalenko@gmail.com>
2024-05-15 13:28:13 +0300

    save

--- src/base/module/base.module.ts
+++ src/base/module/base.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { BaseController } from '../controller/base.controller';
 import { BaseService } from '../service/base.service';
-
+import { BaseMiddleware } from '../middleware/base.middleware';
 
 @Module({
   imports: [],
   controllers: [BaseController],
   providers: [BaseService],
 })
-export class BaseModule {}
+export class BaseModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(BaseMiddleware).forRoutes(BaseController);
+  }
+}
