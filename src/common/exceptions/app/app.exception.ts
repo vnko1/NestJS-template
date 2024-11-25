eedd0552095f3313ad981040355befebc1dd6109
vnko1 <andreyvalenko@gmail.com>
2024-11-25 10:29:48 +0200

    added: base interceptors and decorators

--- src/common/exceptions/app/app.exception.ts
+++
@@ -1,6 +0,0 @@
-import { Catch, ExceptionFilter } from "@nestjs/common";
-
-@Catch()
-export class AppHttpExceptionFilter implements ExceptionFilter {
-  catch() {}
-}
---
+++ src/common/exceptions/httpExceptionFilter/httpExceptionFilter.exception.ts
@@ -0,0 +1,33 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from "@nestjs/common";
+
+import { AppService } from "src/common/services";
+
+@Catch()
+export class HttpExceptionFilter extends AppService implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const response = this.exceptionResponse(host);
+
+    const status = this.status(exception);
+
+    const errorDescription =
+      exception.getResponse && exception.getResponse()["error"];
+
+    if (exception.message)
+      response("Error", exception.message, status, errorDescription);
+    else response(exception.name, exception.message, status);
+  }
+
+  private status(exception: any) {
+    if (exception instanceof HttpException) return exception.getStatus();
+    if (exception instanceof Error) {
+      if (exception.message.startsWith("Validation error")) return 400;
+    }
+    return HttpStatus.INTERNAL_SERVER_ERROR;
+  }
+}
